4792b00f78ad954523b72df8e8fe8a2181e247b1
Mohammedl2002 <kaboo2002@gmail.com>
2018-11-06 05:28:38 -0800

    uploading form and table pages

---
+++ template/form.php
@@ -0,0 +1,63 @@
+
+<!-- INCLUDE NAV & SIDEBAR SECTION -->
+<?php include('layouts/nav-sidebar.php') ?>
+
+
+
+    <!--Start Page Content -->
+    <div class="page-content-wrapper">
+        <div class="container-fluid">
+            
+            <div class="panel-heading">
+                <h1><button type="button" class="btn btn-secondary pull-left">رجوع</button>البحث</h1>
+            </div>
+            
+            <div class="panel-body"><!--Start Body Content-->
+
+                <form>
+                  <div class="form-row">
+                    <div class="col-md-7">
+                        <label for="inputState">حالات</label>
+                        <input type="text" class="form-control" placeholder="Last name">
+                    </div>
+                    <div class="col-md-7">
+                        <label for="inputState">حالات</label>
+                        <input type="text" class="form-control" placeholder="Last name">
+                    </div>
+                    <div class="col-md-7">
+                        <label for="inputState">حالات</label>
+                        <input type="text" class="form-control" placeholder="Last name">
+                    </div>
+                    <div class="col-md-7">
+                        <label for="inputState">حالات</label>
+                        <input type="text" class="form-control" placeholder="Last name">
+                    </div>
+
+                    <div class="form-group col-md-7">
+                      <label for="inputState">حالات</label>
+                      <select id="inputState" class="form-control">
+                        <option selected>إختيار</option>
+                        <option>...</option>
+                      </select>
+                    </div>
+
+                    <div class="custom-file col-md-7">
+                      <input type="file" class="custom-file-input" id="customFile">
+                      <label class="custom-file-label" for="customFile">إختار ملف </label>
+                    </div>
+                    <div class="col-md-7 send">
+                        <button type="submit" class="btn btn-primary mb-4">إرسال</button>
+                        <button type="reset" class="btn btn-primary mb-4">إلغاء</button>
+                    </div>    
+                  </div>
+                </form>
+                   
+            </div><!--Start Body content-->
+        </div>
+    </div> 
+        <!--End Page Content -->
+
+
+
+<!-- INCLUDE FOOTER SECTION -->
+<?php include('layouts/footer.php') ?>
---
+++ template/table.php
@@ -0,0 +1,108 @@
+
+<!-- INCLUDE NAV & SIDEBAR SECTION -->
+<?php include('layouts/nav-sidebar.php') ?>
+
+
+
+    <!--Start Page Content -->
+    <div class="page-content-wrapper">
+        <div class="container-fluid">
+            
+            <div class="panel-heading">
+                <h1><div class="dropdown pull-left">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">إضافة</a>
+                    <a class="dropdown-item" href="#">حذف</a>
+                  </div>
+            </div>
+                الرئيسية</h1>
+            </div>
+            
+            <div class="panel-body"><!--Start Body Content-->
+                <!--Start Form Before Table-->
+
+                <!--End Form Before Table-->
+                <table class="table table-bordered table-striped"><!--Start Table-->
+                    <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col" class="text-center">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                        <button type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa fa-home"></i>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">إضافة</a>
+                                        <a class="dropdown-item" href="#">حذف</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                        <button type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa fa-home"></i>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">إضافة</a>
+                                        <a class="dropdown-item" href="#">حذف</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                        <button type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa fa-home"></i>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">إضافة</a>
+                                        <a class="dropdown-item" href="#">حذف</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table><!--End Table-->
+            </div><!--Start Body content-->
+        </div>
+    </div> 
+        <!--End Page Content -->
+
+
+
+<!-- INCLUDE FOOTER SECTION -->
+<?php include('layouts/footer.php') ?>
