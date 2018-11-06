99d33b5d43c4bd9be51c0a562897ad0598b42bfd
Alkhatib Hamad Adam <alkhatib.hamad@gmail.com>
2018-11-06 18:49:04 +0200

    Update Layouts file footer and nav  and sidebar

--- template/layouts/footer.php
+++ template/layouts/footer.php
@@ -1,7 +1,8 @@
-    </div>
-    <!--End wrapper-->
-    
-    <script src="../js/jquery-1.11.1.min.js"></script>
+    </div> <!--End wrapper-->
+
+</div> <!-- End #App -->
+
+    <script src="../js/app.js"></script>
     <script src="../js/popper.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/main.js"></script>
